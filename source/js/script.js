31e73011affa1f9ebff5cd5c1ce11723cae79a24
whitekz88 <job.beloshapkin@gmail.com>
2024-04-25 20:21:50 +0500

    Дорабатываем скрипт, чтобы без JS меню находилось в потоке

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,21 @@ let navButton = document.querySelector('.button-toggle');
 navList.classList.add('nav__list--hidden');
 navButton.classList.add('button-toggle--closed');
 
+
+if (window.matchMedia('(max-width: 767px)').matches){
+      navList.style.position = "absolute"
+}
+
+
+
+
+
+
+
+document.querySelector('.header__wrapper').style.flexDirection = "row";
+document.querySelector('.header__wrapper').style.alignItems = "center";
+
+
 navButton.addEventListener('click', function () {
   if (navList.classList.contains('nav__list--hidden')) {
     navList.classList.remove('nav__list--hidden');
