397e17865fa904c54b245de3220b9a559fe0d793
whitekz88 <job.beloshapkin@gmail.com>
2024-04-25 20:37:29 +0500

    отменяет предыдущий коммит

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,12 @@
 let navList = document.querySelector('.nav__list');
 let navButton = document.querySelector('.button-toggle');
 
-// navList.classList.remove('navigation__list--no-js');
-// navButton.classList.remove('navigation__button--no-js');
+navList.classList.add('nav__list--hidden');
+navButton.classList.add('button-toggle--closed');
+
+document.querySelector('.header__wrapper').style.flexDirection = "row";
+document.querySelector('.header__wrapper').style.alignItems = "center";
+
 
 navButton.addEventListener('click', function () {
   if (navList.classList.contains('nav__list--hidden')) {
