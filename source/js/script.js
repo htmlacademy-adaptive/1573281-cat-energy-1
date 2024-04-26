18846a39d6c48ec9a0718abf553738d09e499ccd
whitekz88 <job.beloshapkin@gmail.com>
2024-04-26 06:37:46 +0500

    реализует мобильное меню для всех состояний

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,14 @@
 let navList = document.querySelector('.nav__list');
 let navButton = document.querySelector('.button-toggle');
+let headerWrapper = document.querySelector('.header__wrapper');
+let logo = document.querySelector('.logo');
 
-// navList.classList.remove('navigation__list--no-js');
-// navButton.classList.remove('navigation__button--no-js');
+logo.classList.remove('logo--nojs')
+headerWrapper.classList.remove('header__wrapper--nojs')
+headerWrapper.classList.add('container')
+navList.classList.remove('nav__list--nojs');
+navList.classList.add('nav__list--hidden');
+navButton.classList.add('button-toggle--closed');
 
 navButton.addEventListener('click', function () {
   if (navList.classList.contains('nav__list--hidden')) {
